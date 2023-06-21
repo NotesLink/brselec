762681a8047eb6bb5e41bb2009a563d0ed4e1ae5
NotesLink <135240151+NotesLink@users.noreply.github.com>
2023-06-21 16:38:46 +0000

    latest-final

--- script.js
+++ script.js
@@ -13,10 +13,10 @@ function loadForm() {
     let alist = document.getElementsByClassName("nitro-ui-alist")[0];
     alert("Polling Guidelines");
     alert("[1 out of 5] Click on the box containing the name of your favourite participant to vote for them.");
-    alert("[2 out of 5] Select one participant for each role.");
-    alert("[3 out of 5] When you are done selecting the desired participants, recheck your responses and click the \"Submit Response\" button at the end of the page to submit your response.");
-    alert("[4 out of 5] Confirm that you want to submit the form by clicking \"OK\". Then, enter the provided passkey into the passkey prompt. Wait patiently until you see a success message. If you made a mistake entering the passkey, you will see an error message. In that case, you just have to click submit again and repeat the same procedure.");
-    alert("[5 out of 5] If you face any problems, you may ask for help from the teachers.");
+    alert("[2 out of 5] Select one participant for each role. When you are done selecting the desired participants, recheck your responses and click the \"Submit Response\" button at the end of the page to submit your responses.");
+    alert("[3 out of 5] Confirm that you want to submit the form by clicking \"OK\". Then, enter the provided passkey into the passkey prompt.");
+    alert("[4 out of 5] Wait patiently until you see a success message. If you made a mistake entering the passkey, you will see an error message. In that case, you just have to click submit again and repeat the same procedure with the right paskey.");
+    alert("[5 out of 5] Choose carefully because you will have only one vote. If you face any problems, you may ask for help from the teachers.");
     fetch("/res/res.json").then((d) => {
         d.json().then((j) => {
             json = j;
